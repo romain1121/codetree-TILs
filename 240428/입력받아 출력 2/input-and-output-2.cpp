24971de0a82f4242ac99e7bc11ab1401c663d7b6
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-28 17:09:40 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240428/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    string f{}, r{};
+    cin >> f;
+    cin.get();
+    cin >> r;
+
+    cout << f << r << "\n";    
+    return 0;
+}
