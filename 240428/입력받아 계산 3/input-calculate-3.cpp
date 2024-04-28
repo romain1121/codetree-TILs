1aa9eea24e56648132dfdee92f3bc85edb0778b2
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-28 16:52:59 +0900

    Title: 입력받아 계산 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240428/입력받아 계산 3/input-calculate-3.cpp
+++ 240428/입력받아 계산 3/input-calculate-3.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-ios::sync_with_stdio(0);
-cin.tie(0);
-
 int main() {
     // 여기에 코드를 작성해주세요.
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int a{}, b{};
     cin >> a >> b;
 
