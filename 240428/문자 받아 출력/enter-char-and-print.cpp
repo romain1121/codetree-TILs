5efe6d2f2cd61ebeca52139ad08c256a9925ae8f
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-28 16:57:46 +0900

    Title: 문자 받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/문자 받아 출력/enter-char-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char c{};
+    cin >> c;
+
+    cout << c << "\n";
+    return 0;
+}
