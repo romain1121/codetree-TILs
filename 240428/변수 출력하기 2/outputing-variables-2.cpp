b1eac7f9dae4a567f7addfe804f532de8b13efe4
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-28 16:12:29 +0900

    Title: 변수 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    char b = 'C';
+    string str = "!.....!";
+
+    cout << b << str << a;
+    return 0;
+}
