cc93ffde5f6f20d03ff2c3fab90816f1d58bfb91
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-28 17:16:06 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int s{}, f{}, r{};
+    cin >> s;
+    cin.get();
+    cin >> f;
+    cin.get();
+    cin >> r;
+
+    cout << "010-" << r << "-" << f << "\n";
+    return 0;
+}
