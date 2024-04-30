00eebb76a72f38c3b19641c02bf2f2e8f513179f
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-30 23:48:28 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed;
+    cout.precision(1);
+
+    int n{}, i{};
+    double sum{};
+
+    for (; i < 10; i++) {
+        cin >> n;
+        if (!n) {
+            i--;
+            break;
+        }
+        sum += n;
+    }
+
+    cout << int(sum) << " " << sum / double(++i);
+
+    return 0;
+}
