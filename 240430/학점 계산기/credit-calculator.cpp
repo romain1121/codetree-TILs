41e0c37591d25c56bac6723fbb10522fb75ffeb3
5ync <44856597+romain1121@users.noreply.github.com>
2024-04-30 23:12:03 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed;
+    cout.precision(1);
+    
+    int n{};
+    double s{}, input{};
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        cin >> input;
+        s += input;
+    }
+
+    s /= double(n);
+    
+    if (s > 4.0)
+        cout << s << "\nPerfect\n";
+    else if (s > 3.0)
+        cout << s << "\nGood\n";
+    else
+        cout << s << "\nPoor\n";
+
+    return 0;
+}
