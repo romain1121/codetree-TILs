5232bbf3fe160db001bbc5b296d4574aa813ccb4
5ync <44856597+romain1121@users.noreply.github.com>
2024-05-01 00:03:24 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n{}, cnt{};
+    double input[4]{}, avg[10]{};
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> input[0] >> input[1] >> input[2] >> input[3];
+        avg[i] += ((input[0] + input[1] + input[2] + input[3]) / 4.0);
+    }
+    for (int i = 0; i < n; i++) {
+        if (avg[i] >= 60) {
+            cout << "pass\n";
+            cnt++;
+        }
+        else cout << "fail\n";
+    }
+
+    cout << cnt;
+
+    return 0;
+}
