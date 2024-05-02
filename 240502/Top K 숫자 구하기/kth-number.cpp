6764ca87b9a070f6b7af899c033c48d06e4a3c1b
5ync <44856597+romain1121@users.noreply.github.com>
2024-05-02 16:21:40 +0900

    Title: Top K 숫자 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240502/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N{}, k{};
+    cin >> N;
+
+    int *arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    cout << arr[k] << "\n";
+
+    return 0;
+}
